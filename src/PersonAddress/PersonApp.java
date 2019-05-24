b86c6da4fbda077718480a4e33b878cb14d63d0b
maxdhaese <maxdhaese@gmail.com>
2019-05-24 15:53:41 +0200

    commit

--- src/PersonAddress/PersonApp.java
+++ src/PersonAddress/PersonApp.java
@@ -4,16 +4,19 @@ import java.util.Comparator;
 
 public class PersonApp {
     public static void main(String[] args) {
-        Address address1 = new Address(9340,"Melle","KoekoekStraat",70);
-        Person person1 = new Person(28,"Max","D'Haese",address1);
-        Address address2 = new Address(9000,"Gent","TrabotStraat",58);
-        Person person2 = new Person(29,"Tom","Geerts",address2);
+        Address address1 = new Address(9340, "Melle", "KoekoekStraat", 70);
+        Person person1 = new Person(28, "Max", "D'Haese", address1);
+        Address address2 = new Address(9000, "Gent", "TrabotStraat", 58);
+        Person person2 = new Person(29, "Tom", "Geerts", address2);
         NameComperator nameComparator = new NameComperator();
         AgeComparator ageComparator = new AgeComparator();
+        ZipCodeComperator zipCodeComperator = new ZipCodeComperator();
+
+
     }
 
+    public static int showPersonThatCameFirstAfterComparing(Person person1, Person person2,Comparator<Person>comparator){
 
+    }
 
-
-    
 }
